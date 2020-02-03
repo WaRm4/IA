d4d0c8f85fa30dd3cca6a1b95cdc9047680a1871
Killian Meunier <killian.meunier@etu.uca.fr>
2020-02-02 23:32:38 -0500

    ajout du debut du projet

---
+++ IA_manoir/IA_manoir/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IA_manoir
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Rectangle rect;
+            rect = new Rectangle
+            {
+                Stroke = new SolidColorBrush(Colors.Black),
+                Fill = new SolidColorBrush(Colors.Black),
+                Width = 50,
+                Height = 50
+            };
+            Canvas.SetLeft(rect, 35);
+            Canvas.SetTop(rect, 65);
+            lecanvas.Children.Add(rect);
+
+            Image img = new Image
+            {
+                Source = new BitmapImage(new Uri("aspi.png", UriKind.Relative)),
+                Width = 50,
+                Height = 50
+            };
+            Canvas.SetLeft(img, 155);
+            Canvas.SetTop(img, 125);
+            lecanvas.Children.Add(img);
+        }
+
+        private void Poussiere(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bijoux(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Robot(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
